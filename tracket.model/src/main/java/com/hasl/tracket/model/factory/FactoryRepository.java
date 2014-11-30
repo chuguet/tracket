cb86e7cb3bb0d867c5c9dfff1d3614508d9a4493
Huguet <huguet10@gmail.com>
2014-11-30 21:29:10 +0100

    Se arregla la factoria

--- tracket.model/src/main/java/com/hasl/tracket/model/factory/FactoryRepository.java
+++ tracket.model/src/main/java/com/hasl/tracket/model/factory/FactoryRepository.java
@@ -4,18 +4,37 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.stereotype.Component;
+
 import com.hasl.tracket.model.entity.IModelEntity;
 import com.hasl.tracket.model.repository.IRepositoryDAO;
 
-public class FactoryRepository<T extends IModelEntity> implements IFactoryRepository<T>{
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FactoryRepository.
+ *
+ * @param <T>
+ *            the generic type
+ */
+@Component
+public class FactoryRepository<T extends IModelEntity> implements
+		IFactoryRepository<T> {
 
+	/** The repositories dao. */
 	@Inject
 	private List<IRepositoryDAO<T>> repositoriesDAO;
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.hasl.tracket.model.factory.IFactoryRepository#getRepositoryDAO(java
+	 * .lang.Class)
+	 */
 	@Override
 	public IRepositoryDAO<T> getRepositoryDAO(Class<T> clazz) {
-		for(IRepositoryDAO<T> repository : repositoriesDAO){
-			if(clazz.equals(repository.getClass())){
+		for (IRepositoryDAO<T> repository : repositoriesDAO) {
+			if (clazz.equals(repository.getClass())) {
 				return repository;
 			}
 		}
--- tracket.model/src/main/java/com/hasl/tracket/model/factory/IFactoryRepository.java
+++ tracket.model/src/main/java/com/hasl/tracket/model/factory/IFactoryRepository.java
@@ -3,7 +3,21 @@ package com.hasl.tracket.model.factory;
 import com.hasl.tracket.model.entity.IModelEntity;
 import com.hasl.tracket.model.repository.IRepositoryDAO;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface IFactoryRepository.
+ *
+ * @param <T>
+ *            the generic type
+ */
 public interface IFactoryRepository<T extends IModelEntity> {
 
-	IRepositoryDAO<T> getRepositoryDAO (Class<T> clazz);
+	/**
+	 * Gets the repository dao.
+	 *
+	 * @param clazz
+	 *            the clazz
+	 * @return the repository dao
+	 */
+	IRepositoryDAO<T> getRepositoryDAO(Class<T> clazz);
 }
