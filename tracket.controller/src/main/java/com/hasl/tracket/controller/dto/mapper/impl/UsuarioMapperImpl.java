441d43ab4ae932e45387291023b0edfc93f8f3c8
arubibar <arubibar@gmail.com>
2014-12-01 21:26:39 +0100

    Actualizado mapper Usuario

---
+++ tracket.controller/src/main/java/com/hasl/tracket/controller/dto/mapper/impl/UsuarioMapperImpl.java
@@ -0,0 +1,66 @@
+package com.hasl.tracket.controller.dto.mapper.impl;
+
+import com.hasl.tracket.controller.dto.UsuarioDTO;
+import com.hasl.tracket.controller.dto.mapper.IUsuarioMapper;
+import com.hasl.tracket.controller.dto.mapper.factory.MapperType;
+import com.hasl.tracket.model.entity.Usuario;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class UsuarioMapperImpl.
+ */
+public class UsuarioMapperImpl implements IUsuarioMapper {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.hasl.tracket.controller.dto.mapper.IUsuarioMapper#fromUsuarioDTOToUsuario
+	 * (com.hasl.tracket.controller.dto.UsuarioDTO)
+	 */
+	@Override
+	public Usuario fromUsuarioDTOToUsuario(UsuarioDTO usuarioDTO) {
+		Usuario usuario = new Usuario();
+		usuario.setApellidos(usuarioDTO.getApellidos());
+		usuario.setDireccion(usuarioDTO.getDireccion());
+		usuario.setEmail(usuarioDTO.getEmail());
+		usuario.setId(usuarioDTO.getId());
+		usuario.setNombre(usuarioDTO.getNombre());
+		usuario.setPassword(usuarioDTO.getPassword());
+		usuario.setTelefono(usuarioDTO.getTelefono());
+		usuario.setWeb(usuarioDTO.getWeb());
+		return usuario;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.hasl.tracket.controller.dto.mapper.IUsuarioMapper#fromUsuarioToUsuarioDTO
+	 * (com.hasl.tracket.model.entity.Usuario)
+	 */
+	@Override
+	public UsuarioDTO fromUsuarioToUsuarioDTO(Usuario usuario) {
+		UsuarioDTO usuarioDTO = new UsuarioDTO();
+		usuarioDTO.setApellidos(usuario.getApellidos());
+		usuarioDTO.setDireccion(usuario.getDireccion());
+		usuarioDTO.setEmail(usuario.getEmail());
+		usuarioDTO.setId(usuario.getId());
+		usuarioDTO.setNombre(usuario.getNombre());
+		usuarioDTO.setPassword(usuario.getPassword());
+		usuarioDTO.setTelefono(usuario.getTelefono());
+		usuarioDTO.setWeb(usuario.getWeb());
+		return usuarioDTO;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.hasl.tracket.controller.dto.mapper.IMapper#getMapperType()
+	 */
+	@Override
+	public MapperType getMapperType() {
+		return MapperType.USUARIO_MAPPER;
+	}
+
+}
