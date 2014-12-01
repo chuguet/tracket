c6e8b5948ee6e2360f22234aaa7339665b30536d
Huguet <huguet10@gmail.com>
2014-12-01 20:47:18 +0100

    Se implementa el enumerado de los mappers

--- tracket.controller/src/main/java/com/hasl/tracket/controller/dto/mapper/factory/MapperType.java
+++ tracket.controller/src/main/java/com/hasl/tracket/controller/dto/mapper/factory/MapperType.java
@@ -1,5 +1,25 @@
 package com.hasl.tracket.controller.dto.mapper.factory;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Enum MapperType.
+ */
 public enum MapperType {
 
+	/** The administrador mapper. */
+	ADMINISTRADOR_MAPPER,
+	/** The categoria mapper. */
+	CATEGORIA_MAPPER,
+	/** The mayorista mapper. */
+	MAYORISTA_MAPPER,
+	/** The mayorista producto mapper. */
+	MAYORISTA_PRODUCTO_MAPPER,
+	/** The minorista mapper. */
+	MINORISTA_MAPPER,
+	/** The pedido mapper. */
+	PEDIDO_MAPPER,
+	/** The producto mapper. */
+	PRODUCTO_MAPPER,
+	/** The producto pedido mapper. */
+	PRODUCTO_PEDIDO_MAPPER
 }
