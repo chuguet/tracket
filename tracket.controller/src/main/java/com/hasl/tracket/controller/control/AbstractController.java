22fcf930112e2f4d53fe92e39952d78f11f1fca1
arubibar <arubibar@gmail.com>
2014-12-08 20:57:36 +0100

    Actualizada clase AbstractController

--- tracket.controller/src/main/java/com/hasl/tracket/controller/control/AbstractController.java
+++ tracket.controller/src/main/java/com/hasl/tracket/controller/control/AbstractController.java
@@ -1,8 +1,37 @@
 package com.hasl.tracket.controller.control;
 
+import javax.inject.Inject;
+
+import com.hasl.tracket.controller.dto.mapper.factory.IMapperFactory;
+
+// TODO: Auto-generated Javadoc
 /**
  * The Class AbstractController.
  */
 public abstract class AbstractController {
+	
+	/** The mapper factory. */
+	@Inject
+	private IMapperFactory mapperFactory;
+
+	/**
+	 * Gets the mapper factory.
+	 *
+	 * @return the mapper factory
+	 */
+	public IMapperFactory getMapperFactory() {
+		return mapperFactory;
+	}
+
+	/**
+	 * Sets the mapper factory.
+	 *
+	 * @param mapperFactory the new mapper factory
+	 */
+	public void setMapperFactory(IMapperFactory mapperFactory) {
+		this.mapperFactory = mapperFactory;
+	}
+	
+	
 
 }
